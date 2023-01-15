326dc4168ecb65cb7bed6fd22d6d0b58221b416c
Ereglath <kuzmichevjr@gmail.com>
2023-01-15 17:08:32 +0300

    update 1
    DZ completion update, features double points pickup and death menu.

---
+++ Content/C++ scripts/TestClass.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Username on 12/01/2023.
+//
+
+#include "TestClass.h"
---
+++ Content/C++ scripts/TestClass.h
@@ -0,0 +1,14 @@
+//
+// Created by Username on 12/01/2023.
+//
+
+#ifndef MYPROJECT_TESTCLASS_H
+#define MYPROJECT_TESTCLASS_H
+
+
+class TestClass {
+
+};
+
+
+#endif //MYPROJECT_TESTCLASS_H
